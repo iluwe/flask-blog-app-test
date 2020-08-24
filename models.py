f3036b464c2ee35a8f226f30b7c055ee5c9a6703
iluwe <iluwailuhin@gmail.com>
2020-08-24 14:46:52 +0300

    Add Tag table to db.

--- models.py
+++ models.py
@@ -25,3 +25,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}>'
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    slug = db.Column(db.String(50))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Tag id: {self.id}, name: {self.name}>'
